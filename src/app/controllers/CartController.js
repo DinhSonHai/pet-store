cff937f8911883d2a15a113a5d374d606c3c0a1d
DinhSonHai <sdinhhai@gmail.com>
2020-11-03 15:16:52 +0700

    Add and handle get all product in cart route

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -4,21 +4,51 @@ const User = require('../models/User');
 const Cart = require('../models/Cart');
 
 class CartController {
-  // @route   GET api/cart/add
+  // @route   GET api/cart
+  // @desc    Lấy tất cả sản phẩm trong giỏ hàng của một người dùng
+  // @access  Public
+  async index(req, res) {
+    //Lấy token từ header
+    const token = req.header('x-auth-token');
+    // Lấy giỏ hàng
+    try {
+      const cart = await Cart.find({ userId: req.user._id }).populate('productId', [
+        'productName',
+        'price'
+      ]);
+      return res.json(cart);
+    } catch (error) {
+      return res.status(500).send('Server Error');
+    }
+  }
+
+  // @route   POST api/cart/add
   // @desc    Add product to cart
   // @access  Public
   async addCart(req, res) {
     //Lấy token từ header
     const token = req.header('x-auth-token');
-    // Thêm giỏ hàng vào local storage
+    // Thêm vào giỏ hàng
     try {
-      const { productId, quantity } = req.body;
-      let cart = new Cart({
-        userId: req.user._id,
-        productId,
-        quantity
-      });
-      await cart.save();
+      let { productId, quantity } = req.body;
+      let cart = await Cart.findOne({ userId: req.user._id, productId });
+      if(cart) {
+        quantity += cart.quantity;
+        cart = await Cart.findOneAndUpdate(
+          { userId: req.user._id, productId },
+          { $set: { quantity }},
+          //new: true sẽ trả về đối tượng đã được cập nhật
+          { new: true }
+        )
+      }
+      else {
+        cart = new Cart({
+          userId: req.user._id,
+          productId,
+          quantity
+        });
+        await cart.save();
+      }
       return res.json({ msg: 'Thêm vào giỏ hàng thành công'})
     } catch (error) {
       return res.status(500).send('Server Error');
--- src/routes/api/cart.js
+++ src/routes/api/cart.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const CartController = require('../../app/controllers/CartController');
 const auth = require('../../app/middlewares/auth');
 
-// @route   GET api/cart/addcart
+// @route   GET api/cart/getcart
+// @desc    Lấy tất cả sản phẩm trong giỏ hàng của một người dùng
+// @access  Public
+router.get('/', auth, CartController.index);
+
+// @route   POST api/cart/addcart
 // @desc    Thêm sản phẩm vào giỏ hàng
 // @access  Public
 router.post('/addcart', auth, CartController.addCart);
