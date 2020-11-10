ef520eb22de7a96818676d414bc982f1c41e93d8
DinhSonHai <sdinhhai@gmail.com>
2020-11-10 18:38:30 +0700

    Add mongoose-delete to category, type, user model

--- server/app/models/Type.js
+++ server/app/models/Type.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dayjs = require('dayjs');
 const now = dayjs();
+const mongooseDelete = require('mongoose-delete');
 
 const Schema = mongoose.Schema;
 
@@ -8,6 +9,11 @@ const TypeSchema = new Schema({
   typeName: { type: String, trim: true },
   categoryId: { type: Schema.Types.ObjectId, ref: 'category' },
   createdAt: { type: Date, default: now.toISOString() },
+});
+
+TypeSchema.plugin(mongooseDelete, {
+  deletedAt: true,
+  overrideMethods: 'all',
 });
 
 module.exports = mongoose.model('type', TypeSchema);
