6efa6faa73d086a01d858d043c78f1b0623c75a2
DinhSonHai <sdinhhai@gmail.com>
2021-07-13 23:08:45 +0700

    fix heroku router

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ route(app);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(process.cwd(), 'client', 'build', 'index.html'));
   });
 }
 
